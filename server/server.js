4c0c61bbddd7405c2e02723415c640a0019d4e0a
roboticsound <roboticsound@tutanota.com>
2018-01-27 23:34:16 +0000

    Add form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,23 +15,15 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app!',
-        createdAt: new Date().getTime()
-    });
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user has joined',
-        createdAt: new Date().getTime()
-    });
-    socket.on('createMessage', (message) => {
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined'));
+
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This from the server.');
     });
 
     socket.on('disconnect', () => {
